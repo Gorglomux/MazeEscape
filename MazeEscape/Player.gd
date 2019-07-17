49d63517a8817e63b8249d2e16c95b0384475c15
Bastien Doret <bastien.doret@etudiant.univ-lr.fr>
2019-07-17 14:35:04 +0200

    tourner_dans_le_vide

--- MazeEscape/Player.gd
+++ MazeEscape/Player.gd
@@ -17,12 +17,16 @@ func _physics_process(delta):
 			match(d):
 				DIRECTION.HAUT:
 					position.y = position.y - 50
+					global_rotation_degrees = 180
 				DIRECTION.BAS:
 					position.y = position.y + 50
+					global_rotation_degrees = 0
 				DIRECTION.GAUCHE:
 					position.x = position.x - 50
+					global_rotation_degrees = 90
 				DIRECTION.DROITE:
 					position.x = position.x + 50
+					global_rotation_degrees = 270
 			if(next_d != -1):
 				d = next_d
 				next_d = -1
